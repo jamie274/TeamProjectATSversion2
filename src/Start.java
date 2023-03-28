43eef6b9eba01ef56b0538f33c26f3b720a6a8b2
jamie274 <jamie-lee.gordon@city.ac.uk>
2023-03-28 20:36:28 +0100

    commit 2

--- src/Start.java
+++ src/Start.java
@@ -5,8 +5,6 @@ public class Start {
     public static void main(String[] args) {
         // Starts the program at the welcome page
         new WelcomePage().setVisible(true);
-
-        //new OfficeManagerMenu("21Savage","Abdul").setVisible(true);
     }
 }
 
